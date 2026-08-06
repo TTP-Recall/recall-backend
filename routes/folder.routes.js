e233d5294280a009b48999f6acbcf9dbe17f944d
YeffryF <yeffry.fermin.ttp@gmail.com>
2026-08-06 15:10:15 -0400

    add boiler plater for folder routes

--- routes/folder.routes.js
+++ routes/folder.routes.js
@@ -1,3 +1,29 @@
+const express = require('express')
+const router = express.Router()
 
+// Get all folders
+router.get('/', async (req, res) => {
 
-//
+})
+
+// Get single folder
+router.get('/:id', async (req, res) => {
+
+})
+
+// Create folder
+router.post('/', async (req, res) => {
+
+})
+
+// Update folder name
+router.patch('/:id', async (req, res) => {
+
+})
+
+// Delete folder
+router.delete('/:id', async (req, res) => {
+
+})
+
+module.exports = router
